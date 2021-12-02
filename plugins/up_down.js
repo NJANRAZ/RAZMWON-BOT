3f07dd871fdc59f9154ea402cc5e35a3d7d8e1d3
RAZ <92934300+NJANRAZ@users.noreply.github.com>
2021-12-02 20:29:24 +0530

    Create up_down.js

---
+++ plugins/up_down.js
@@ -0,0 +1,15 @@
+/* Copyright (C) 2021 Raz Ayman.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+Razmwon - Raz Ayman
+*/
+
+const Asena = require('../events');
+const {MessageType, Mimetype} = require('@adiwajshing/baileys');
+const got = require('got')
+const fs = require('fs')
+const Language = require('../language');
+/*const Lang = Language.getString('up_down');
+Asena.addCommand({pattern: 'download ?(.*)', fromMe: true, desc: Lang.MEMES_DESC, usage: 'meme top;bottom'}, (async (message, match) => {    
+    if (message.reply_message === false) return await message.sendMessage(Lang.NEED_REPLY);
+}));*/
